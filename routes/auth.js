ba7c8cbea69a662db381e7e7ae6a40411967fb53
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-21 16:44:52 +0530

    validation added for authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+router.post('/', 
+[    
+    body('name', "Please enter at least 3 characters.").isLength({min:3}),    
+    body('email').isEmail(),    
+    body('password', "Please enter at least 5 characters.").isLength({min:5})
+], 
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then(user => res.json(user))
+        .catch(err => {
+        res.json({error: "User with this email already exists. Please enter a unique value of email.", message: err.message})})
 })
 
 module.exports = router
