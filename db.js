3f56997b9c3c077fde795bc2dc68e58b233882cb
BurhanuddinRaja <mails4burhan@gmail.com>
2022-07-01 17:14:08 +0530

    heroku deploy

--- db.js
+++ db.js
@@ -8,12 +8,19 @@ const mongoURI = process.env.MONGODB_URI
 const connectToMongoDb = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo")
+    }, {
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+        replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
     }, (err) => {
         if (err) return console.log("Error: ", err);
         console.log(
             "MongoDB Connection -- Ready state is:",
             mongoose.connection.readyState)
-        })
+    })
 }
 
 module.exports = connectToMongoDb
